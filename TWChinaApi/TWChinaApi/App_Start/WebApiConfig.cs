f484b2751e006109ae6303cca51737cfbc603819
Bojian Chen <bjchen@thoughtworks.com>
2015-12-07 20:04:15 +0800

    add twchina api project template

---
+++ TWChinaApi/TWChinaApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+
+namespace TWChinaApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute("TWChinaApi", "api/{controller}/{action}/{id}",
+                new {id = RouteParameter.Optional}
+                );
+
+            config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new {id = RouteParameter.Optional}
+                );
+        }
+    }
+}
---
+++ TWChinaApi/TWChinaApi/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace TWChinaApi.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        public string Home()
+        {
+            return "home in home controller";
+        }
+         
+    }
+}
